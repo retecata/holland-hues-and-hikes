a5d2b74316eb460c9ca5f2fe226fc55aeb63cd5e
Catalina <catalinarete@gmail.com>
2025-07-30 22:53:18 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/holland-hues-and-hikes">
         <AppRoutes />
       </BrowserRouter>
     </TooltipProvider>
